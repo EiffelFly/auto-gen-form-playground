2390f400b2b38aed9179173517f6563704f18001
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-10-20 23:12:00 +0800

    feat: recursively reset value

---
+++ src/lib/transform/recursivelyResetFormData.ts
@@ -0,0 +1,53 @@
+import { GeneralRecord, dot } from "@instill-ai/toolkit";
+import { InstillFormTree, SelectedConditionMap } from "../type";
+
+export function recursivelyResetFormData(
+  resetTree: InstillFormTree,
+  resetSelectedConditionMap: SelectedConditionMap,
+  formData: GeneralRecord
+) {
+  switch (resetTree._type) {
+    case "formGroup": {
+      for (const property of resetTree.properties) {
+        recursivelyResetFormData(property, resetSelectedConditionMap, formData);
+      }
+      break;
+    }
+    case "formArray": {
+      for (const item of resetTree.properties) {
+        recursivelyResetFormData(item, resetSelectedConditionMap, formData);
+      }
+      break;
+    }
+    case "formCondition": {
+      const constField = resetTree.conditions[
+        Object.keys(resetTree.conditions)[0]
+      ].properties.find((e) => "const" in e);
+
+      if (constField && constField.path) {
+        const selectedCondition = resetSelectedConditionMap[constField.path];
+
+        console.log(selectedCondition);
+
+        if (selectedCondition) {
+          recursivelyResetFormData(
+            resetTree.conditions[selectedCondition],
+            resetSelectedConditionMap,
+            formData
+          );
+        }
+      }
+
+      break;
+    }
+    default: {
+      if ("const" in resetTree) {
+        break;
+      }
+
+      if (resetTree.path) {
+        dot.setter(formData, resetTree.path, null);
+      }
+    }
+  }
+}
